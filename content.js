121162be67fa527bbe2c156da8adf79630f491cb
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2022-09-03 21:23:44 +0200

    Split up methods and add new logo with fire

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
 
 burnMap = new Map([
     ['#01_Fake Heels', { 'burned': 225, 'minted': 500 }],
-    ['#05_Kostika',  { 'burned': 258, 'minted': 500 }],
+    ['#05_Kostika', { 'burned': 258, 'minted': 500 }],
     ['#04_Draconya', { 'burned': 255, 'minted': 500 }]
 ])
 
@@ -10,30 +10,44 @@ function supply(item) {
     return item.minted - item.burned
 }
 
-// Trying to not get an infinite loop here
-var attempts = 0
-async function burnAdjust() {
-    while (attempts < 100) {
+async function getItemNameFromHeading() {
+    while (true) {
         var headers = document.getElementsByTagName("h1")
         heading = headers[0]
-        if(heading !== undefined) {
-            itemName = heading.innerHTML
-            var itemDetails = burnMap.get(itemName)
-
-            if (itemDetails === undefined) {
-                break;
-            } 
-            query = 'span:contains("/minted available")'.replace("minted", itemDetails.minted)
-            var targetSpan = $(query)
-            if(targetSpan[0] !== undefined && targetSpan[0].innerHTML.indexOf(itemDetails.minted) != -1) {
-                var updateValue = targetSpan[0]
-                var newValue = updateValue.innerHTML.replace(itemDetails.minted, supply(itemDetails))
-                updateValue.innerHTML = newValue
-                break;
-            }
-        } 
-        await new Promise(r => setTimeout(r, 100));
-        attempts++;
+        if (heading === undefined) {
+            await new Promise(r => setTimeout(r, 100));
+        } else {
+            return heading.innerHTML
+        }
     }
 }
+
+async function getEditonsElement(itemDetails) {
+    query = 'span:contains("/minted available")'.replace("minted", itemDetails.minted)
+    while (true) {
+        
+    }
+}
+
+function hasMintedQuantity(targetSpan, itemDetails) {
+    return targetSpan[0].innerHTML.indexOf(itemDetails.minted) != -1;
+}
+
+async function burnAdjust() {
+    itemName = await getItemNameFromHeading()
+    var itemDetails = burnMap.get(itemName)
+
+
+    
+    query = 'span:contains("/minted available")'.replace("minted", itemDetails.minted)
+    var targetSpan = $(query)
+    if (targetSpan[0] !== undefined && hasMintedQuantity(targetSpan, itemDetails)) {
+        var updateValue = targetSpan[0]
+        var newValue = updateValue.innerHTML.replace(itemDetails.minted, supply(itemDetails))
+        updateValue.innerHTML = newValue
+    }
+}
+
 burnAdjust()
+
+
