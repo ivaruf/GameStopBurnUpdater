e2aad83150f64faca304cd1d328d579a803f3b19
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2022-09-04 07:12:17 +0200

    Move burn map to own file, part 1

--- content.js
+++ content.js
@@ -1,13 +1,7 @@
 // content.js
 
 // Tasks
-// TODO put burn map in separate file, that loads latest version (even if plugin is old)
 // TODO get rid of jQuery?
-GameStopTokenBurnMap = new Map([
-    ['0x3b66eead9d7f4d57d7f33aa3a5e27971fef9d8381ccdde8792bc2e2b084aac56', { 'burned': 225, 'minted': 500 }], // #01_Fake Heels
-    ['0x11087ebd3afcc64d149baab2a843a291a35dab35e046d89d9659bf1fc0f50f84', { 'burned': 258, 'minted': 500 }], // #05_Kostika
-    ['0x6e364e871068cb5b0150266ea7c39a458cc026b393b2049f4f31a9a910d60e9c', { 'burned': 255, 'minted': 500 }] // #04_Draconya
-])
 
 function supply(item) {
     return item.minted - item.burned
